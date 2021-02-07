15c6228c4c014ddc3dd4e3a91bc9bdce8e8303be
LatoriMiller <lamiller@gmail.com>
2021-02-07 08:30:01 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+var slideIndex = 0;
+showSlides();
+// showSlides function will access the mySlides and dot elements from the home page's DOM and use a loop to change the display property to none every 5 seconds. SlideIndex is then incremented. 
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); 
+}
