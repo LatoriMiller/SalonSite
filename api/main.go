40afcd5024d7491581c743e18284e983b12c12dd
LatoriMiller <lamiller@gmail.com>
2021-02-11 10:18:52 -0500

    restructure app for docker

---
+++ api/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+type product struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Price       string `json:"price`
+	Image       string `json:"image"`
+	Category    string `json:"category"`
+	Description string `json:"description"`
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+var db *sql.DB
+
+func main() {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/products", fetchProducts).Methods("GET")
+
+	log.Println("listening on port 8000")
+
+	http.ListenAndServe(":8000", router)
+}
+
+func fetchProducts(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	db, err := sql.Open("mysql", "root:Blessing1!@tcp(database:3306/products)")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	var products []product
+	// create a variable for the sql query
+	query := "select id, name, price, image, category, description from products"
+	results, err := db.Query(query)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	// loop thru each item in the database and map into the struct format
+	for results.Next() {
+		var product product
+		err := results.Scan(&product.ID, &product.Name, &product.Price, &product.Image, &product.Category, &product.Description)
+
+		if err != nil {
+			panic(err.Error())
+		}
+		//appends each product e.g. id/ individual item from the scan as it loops through to the product slice
+		products = append(products, product)
+	}
+	//sets the page status to 200
+	w.WriteHeader(http.StatusOK)
+	//let the browser know we are sending json
+	w.Header().Set("Content-Type", "application/json")
+	//converts the product slice into json
+	json.NewEncoder(w).Encode(products)
+
+}
---
+++ database/products.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE products;
+USE products;
+
+CREATE TABLE products (name VARCHAR(20), description VARCHAR(120), price DECIMAL, image VARCHAR(120), category VARCHAR(30));
+
+INSERT INTO products (name, description, price, image, category)
+VALUES 
+("Salon Tools", "Salon Latori ipsum lorem. Lorem ipsum lorem haircare", 300.00, "images/salonTools.jpg", "tools"),
+("Shampoo", "Salon Latori ipsum lorem. Lorem ipsum lorem haircare", 20.00, "images/shampoo.jpg", "haircare"),
+("Eyeliner", "Salon Latori ipsum lorem. Lorem ipsum lorem haircare", 30.00, "images/redPencils.jpg", "makeup"),
+("Brush Set", "Salon Latori ipsum lorem. Lorem ipsum lorem haircare", 130.00, "images/blueBrushset.jpg", "makeup"),
+("Premium Eyeliner", "Salon Latori ipsum lorem. Lorem ipsum lorem haircare", 24.00, "images/goldEyeliner.jpg", "makeup"),
+("Gift Set", "Salon Latori ipsum lorem. Lorem ipsum lorem haircare", 120.00, "images/giftset.jpg", "gift"),
+("Body Oil", "Salon Latori ipsum lorem. Lorem ipsum lorem haircare", 30.00, "images/bodyOil.jpg", "skincare"),
+("Eyeshadow", "Salon Latori ipsum lorem. Lorem ipsum lorem haircare", 90.00, "images/eyeshadow.jpg", "makeup"),
+("Foundation", "Salon Latori ipsum lorem. Lorem ipsum lorem haircare", 39.00, "images/foundation.jpg", "makeup"),
+("Crystal Facial Roller", "Salon Latori ipsum lorem. Lorem ipsum lorem haircare", 28.00, "https://s7d5.scene7.com/is/image/Anthropologie/48551378_030_b?$a15-pdp-detail-shot$&fit=constrain&qlt=80&wid=100", "skincare"),
+("Dry Shampoo", "Salon Latori ipsum lorem. Lorem ipsum lorem haircare", 20.00, "https://s7d5.scene7.com/is/image/Anthropologie/54415401_010_b?$a15-pdp-detail-shot$&fit=constrain&qlt=80&wid=100", "haircare"),
+("Curling Cream", "Salon Latori ipsum lorem. Lorem ipsum lorem haircare", 30.00, "https://s7d5.scene7.com/is/image/Anthropologie/59450478_010_b?$a15-pdp-detail-shot$&fit=constrain&qlt=80&wid=100", "haircare");
